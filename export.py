cb44e0e2168f828e02a677ed55e8d851c63c3fe1
kristian <lange.kristian@gmail.com>
2024-09-28 10:56:27 +0200

    Change export.py to new db schema and keep old export script under export_oldschema.py

--- export.py
+++ export.py
@@ -1,6 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+Export a study's data into a file with the format CSV
+
+Usage: python export.py studyId
+Parameter: studyId - the ID of the study
+"""
+
 import mysql.connector
 import sys, inspect, csv, json
 from datetime import datetime
+
+DB_HOST = "localhost"
+DB_USER = "ommuser"
+DB_PASSWORD = "abc123"
+DB_DATABASE = "omm"
 
 # Parses the JSON string that belongs to the given key of a dictionary and turns it into a nested dictionary.
 # If the value is 'null' the element belonging to the key is removed.
@@ -21,72 +35,45 @@ def flatten(my_dict):
             result[key] = value
     return result
 
+# Writes the given data to a file in CSV format
+def write_to_file(rows):
+    filename = "data_" + datetime.today().strftime('%Y%m%d%H%M%S') + ".csv"
+    with open(filename, "w", newline="") as f:
+        w = csv.DictWriter(f, rows[0].keys())
+        w.writeheader()
+        for row in rows:
+            print(row)
+            w.writerow(row)
+
+
 # Check we have the study ID as an argument
 if len(sys.argv) <= 1:
-    print("missing argument study ID")
+    print("Missing argument study ID")
     sys.exit()
 studyId = sys.argv[1]
 
-dataBase = mysql.connector.connect(
-  host ="localhost",
-  user ="ommuser",
-  passwd ="abc123",
-  database = "omm_live"
-)
- 
+
+
+dataBase = mysql.connector.connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE)
 cursorObject = dataBase.cursor(dictionary=True)
 
-# const jobVariables = Database.select('jobs.id as job_id', aggVars)
-#   .from('jobs')
-#   .leftJoin('job_variable', 'jobs.id', 'job_variable.job_id')
-#   .leftJoin('variables', 'variables.id', 'job_variable.variable_id')
-#   .where('jobs.study_id', this.id)
-#   .groupBy('jobs.id')
-
-#    return await Database
-#      .select(
-#        'jobs.id as job_id', 'jobs.position', 'jobs.study_id',
-#        'job_statuses.name as status', 'participants.identifier as participant',
-#        'job_states.data', 'job_variables.trial_vars', 'participants.meta')
-#      .from('jobs')
-#      .leftJoin('job_states', 'jobs.id', 'job_states.job_id')
-#      .leftJoin('participants', 'job_states.participant_id', 'participants.id')
-#      .leftJoin('job_statuses', 'job_states.status_id', 'job_statuses.id')
-#      .leftJoin(jobVariables.as('job_variables'), 'jobs.id', 'job_variables.job_id')
-#      .where('jobs.study_id', this.id)
-#      .whereNotNull('job_states.data')
-
 query = f'''
-SELECT 
-    jobs.id AS 'job_id', jobs.position, jobs.study_id, 
-    job_statuses.name AS status, participants.identifier AS participant,
-    participants.meta, job_states.data, job_variable.value
-FROM jobs
-LEFT JOIN job_states ON jobs.id = job_states.job_id AND job_states.data IS NOT NULL
-LEFT JOIN participants ON job_states.participant_id = participants.id
-LEFT JOIN job_statuses ON job_states.status_id = job_statuses.id
-LEFT JOIN job_variable ON jobs.id = job_variable.job_id
-WHERE jobs.study_id = {studyId} AND job_states.data IS NOT NULL
-;
-'''
+SELECT
+    study_id, name, data
+FROM job_results
+LEFT JOIN studies ON job_results.study_id = studies.id
+WHERE study_id = {studyId};
+;'''
 
 cursorObject.execute(query)
+
 rows = cursorObject.fetchall()
 
-# Parse JSON elements 'meta' and 'data'
-rows = map(lambda d: parse_json_element(d, 'meta'), rows)
-#rows = map(lambda d: parse_json_element(d, 'data'), rows)
+# Parse JSON element 'data'
+rows_data_parsed = map(lambda d: parse_json_element(d, 'data'), rows)
 # Flatten the dictionary
-rows_cleaned = list(map(flatten, rows))
-
+rows_flattened = list(map(flatten, rows_data_parsed))
 # Write data to CSV file
-filename = "data_" + datetime.today().strftime('%Y%m%d%H%M%S') + ".csv"
-with open(filename, "w", newline="") as f:
-    w = csv.DictWriter(f, rows_cleaned[0].keys())
-    w.writeheader()
-    for row in rows_cleaned:
-        print(row)
-        w.writerow(row)
+write_to_file(rows_flattened)
 
 dataBase.close()
-
